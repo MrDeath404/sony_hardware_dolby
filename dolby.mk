62f88a8890495458858091cb25258c45fbc9a69a
MrDeath404 <pisarekpatryk404@gmail.com>
2025-05-04 18:44:55 +0200

    Changed out dir from vendor to system and removed doubled vendor in path

--- dolby.mk
+++ dolby.mk
@@ -1,18 +1,17 @@
 # dolby.mk path
 LOCAL_PATH := hardware/dolby
+DOLBY_PATH := $(LOCAL_PATH)/dolby_manager
 
 # Add dolby path to soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH)
 
-DOLBY_PATH := $(LOCAL_PATH)/dolby_manager
-
 # Import dolby manager permissions etc.
 PRODUCT_COPY_FILES += \
-	$(DOLBY_PATH)/etc/default-permissions/default-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default-permissions/default-com.dolby.daxservice.xml \
-	$(DOLBY_PATH)/etc/permissions/privapp-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-com.dolby.daxservice.xml \
-	$(DOLBY_PATH)/etc/sysconfig/config-com.dolby.daxappui2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/config-com.dolby.daxappui2.xml \
-	$(DOLBY_PATH)/etc/sysconfig/config-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/config-com.dolby.daxservice.xml
+	$(DOLBY_PATH)/etc/default-permissions/default-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-com.dolby.daxservice.xml \
+	$(DOLBY_PATH)/etc/permissions/privapp-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-com.dolby.daxservice.xml \
+	$(DOLBY_PATH)/etc/sysconfig/config-com.dolby.daxappui2.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/config-com.dolby.daxappui2.xml \
+	$(DOLBY_PATH)/etc/sysconfig/config-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/config-com.dolby.daxservice.xml
 
 # Sony Dolby Manger
 PRODUCT_PACKAGES += \
@@ -25,8 +24,8 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
 
 # Dolby configs
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/vendor/etc/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/vendor/etc/dolby/dax-default.xml \
-	$(LOCAL_PATH)/vendor/etc/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/vendor/etc/media_codecs_dolby_audio.xml
+	$(LOCAL_PATH)/vendor/etc/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+	$(LOCAL_PATH)/vendor/etc/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
 
 # Dolby properties
 PRODUCT_VENDOR_PROPERTIES += \
@@ -34,7 +33,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # System init files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc
+	$(LOCAL_PATH)/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc
 
 # Dolby libs
 PRODUCT_PACKAGES += \
